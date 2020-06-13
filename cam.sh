24625776df8d3d69395f974f9107f139b86d0481
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2020-06-13 14:57:15 +0200

    fix lag issue

--- cam.sh
+++ cam.sh
@@ -1,3 +1,4 @@
 ls /dev/vid*
 read -p 'Enter camera: ' cam_id
-mpv $cam_id --vf=hflip --glsl-shader=shader.glsl gpu-api=vulkan
+mpv $cam_id --vf=hflip --no-cache --aid=no --profile=low-latency --video-sync=desync
+# --glsl-shader=shader.glsl gpu-api=vulkan
